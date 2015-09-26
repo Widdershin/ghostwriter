4762e5d4b700d410fc9c83a9bdff6b5438750362
Nick Johnstone <ncwjohnstone@gmail.com>
2015-09-26 22:14:44 +1200

    Set up time travelling

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 import {run, Rx} from '@cycle/core';
 import {makeDOMDriver, h} from '@cycle/dom';
 
+import CycleTimeTravel from 'cycle-time-travel';
+
+function log (label) {
+  return console.log.bind(console, label);
+}
+
+function keyPressed (key) {
+  return (ev) => ev.key === key || ev.keyIdentifier === key;
+}
+
 function main ({DOM}) {
-  const editorText$ = Rx.Observable.just('');
+  const editorText$ = DOM.select('.text').events('input')
+    .map(event => event.target.value)
+    .debounce(300)
+    .startWith('');
+
+  const keyPress$ = Rx.Observable
+    .fromEvent(document.body, 'keypress')
+    .map(ev => ev.key);
+
+  const tabPress$ = keyPress$.filter(keyPressed('Tab'));
+
+  const timeTravel = CycleTimeTravel(DOM, [
+    {stream: editorText$, label: 'editorText$'},
+    {stream: keyPress$, label: 'keyPress$'},
+    {stream: tabPress$, label: 'tabPress$'}
+  ]);
+
   return {
-    DOM: editorText$.map(text => (
-      h('textarea.text', text)
-    ))
+    DOM: Rx.Observable.combineLatest(
+      timeTravel.timeTravel.editorText$,
+      timeTravel.DOM,
+      (text, timeTravelBar) => (
+        h('.app-inner', [
+          h('textarea.text', {rows: 20, cols: 50, value: text}),
+          timeTravelBar
+        ])
+      ))
   };
 }
 
