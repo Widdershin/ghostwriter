09d87f66e9409c573e5cd5963382623c0ca351f0
Nick Johnstone <ncwjohnstone@gmail.com>
2015-09-26 22:28:54 +1200

    Start adding rhyme-complete

--- index.js
+++ index.js
@@ -8,20 +8,32 @@ function log (label) {
 }
 
 function keyPressed (key) {
-  return (ev) => ev.key === key || ev.keyIdentifier === key;
+  return ev => ev.key === key || ev.keyIdentifier === key;
+}
+
+function addRhyme (text) {
+  return text + ' bluh!';
+}
+
+function updateText (text) {
+  return oldText => text;
 }
 
 function main ({DOM}) {
-  const editorText$ = DOM.select('.text').events('input')
-    .map(event => event.target.value)
-    .debounce(300)
-    .startWith('');
-
   const keyPress$ = Rx.Observable
     .fromEvent(document.body, 'keypress')
     .map(ev => ev.key);
 
   const tabPress$ = keyPress$.filter(keyPressed('Tab'));
+
+  const textUpdate$ = DOM.select('.text').events('input')
+    .map(event => event.target.value)
+    .startWith('');
+
+  const editorText$ = Rx.Observable.merge(
+    tabPress$.map(log('tab')).map(ev => addRhyme),
+    textUpdate$.map(text => updateText(text))
+  ).scan((text, modifier) => modifier(text), '');
 
   const timeTravel = CycleTimeTravel(DOM, [
     {stream: editorText$, label: 'editorText$'},
