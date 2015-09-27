45692db7c90d65614deab38bc0afb078317d77e2
Nick Johnstone <ncwjohnstone@gmail.com>
2015-09-27 14:16:06 +1300

    Rhyme autocompletion!

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import {run, Rx} from '@cycle/core';
 import {makeDOMDriver, h} from '@cycle/dom';
-
 import CycleTimeTravel from 'cycle-time-travel';
+import $ from 'jquery';
+import _ from 'lodash';
+import rhyme from 'rhyme';
+
+Rx.config.longStackSupport = true;
 
 function log (label) {
   return console.log.bind(console, label);
@@ -11,8 +15,8 @@ function keyPressed (key) {
   return ev => ev.key === key || ev.keyIdentifier === key;
 }
 
-function addRhyme (text) {
-  return text + ' bluh!';
+function addRhyme (wordToRhyme, rhyme) {
+  return text => text + _.sample(rhyme.rhyme(wordToRhyme)).toLowerCase();
 }
 
 function updateText (text) {
@@ -20,35 +24,53 @@ function updateText (text) {
 }
 
 function main ({DOM}) {
-  const keyPress$ = Rx.Observable
-    .fromEvent(document.body, 'keypress');
-
-  const tabPress$ = keyPress$.filter(keyPressed('Tab'));
-
-  tabPress$.forEach(ev => ev.preventDefault());
-
   const textUpdate$ = DOM.select('.text').events('input')
     .map(event => event.target.value)
     .startWith('');
 
+  const keyPress$ = Rx.Observable
+    .fromEvent(document.body, 'keypress');
+
+  const tabPress$ = Rx.Observable.merge(
+    keyPress$.filter(keyPressed('Tab')),
+    DOM.select('.rhyme').events('click')
+  );
+
+  const wordToRhyme$ = textUpdate$.map(
+    text => _.chain(text.split('\n'))
+      .map(_.trim)
+      .select(line => line !== '')
+      .map(line => line.split(' '))
+      .thru(lines => {
+        if (lines.length > 1) {
+          return lines[lines.length - 2];
+        }
+
+        return _.last(lines);
+      })
+      .last().value()
+  ).debounce(300);
+
+  const rhyme$ = Rx.Observable.fromCallback(rhyme)();
+
+  tabPress$.forEach(ev => ev.preventDefault());
+
   const editorText$ = Rx.Observable.merge(
-    tabPress$.map(ev => addRhyme),
+    tabPress$.withLatestFrom(wordToRhyme$, rhyme$, (ev, wordToRhyme, rhyme) => addRhyme(wordToRhyme, rhyme)),
     textUpdate$.map(text => updateText(text))
   ).scan((text, modifier) => modifier(text), '');
 
   const timeTravel = CycleTimeTravel(DOM, [
-    {stream: editorText$, label: 'editorText$'},
-    {stream: keyPress$.map(ev => ev.key), label: 'keyPress$'},
-    {stream: tabPress$.map(_ => 'Tab'), label: 'tabPress$'}
   ]);
 
   return {
     DOM: Rx.Observable.combineLatest(
-      timeTravel.timeTravel.editorText$,
+      editorText$,
       timeTravel.DOM,
       (text, timeTravelBar) => (
         h('.app-inner', [
           h('textarea.text', {rows: 20, cols: 50, value: text}),
+          h('button.rhyme', 'Rhyme'),
           timeTravelBar
         ])
       ))
@@ -59,4 +81,4 @@ const drivers = {
   DOM: makeDOMDriver('.app')
 };
 
-run(main, drivers);
+$(() => run(main, drivers));
