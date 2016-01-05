809a2dcc3ca686a20b3af8398c06bbdff15368a3
Raquel Moss <raquelxmoss@gmail.com>
2016-01-05 19:51:40 +1300

    Allow user to select a rhyme with their mouse

---
+++ src/actions/choose-suggestion.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+
+import { appendNextRhyme } from '../helpers';
+
+export default function chooseSuggestion (event) {
+  return state => {
+    const clickedRhyme = event.target.innerText;
+    const text = appendNextRhyme(false, state.text, clickedRhyme);
+    const selectedRhymeIndex = chooseRhymeAtIndex(state.availableRhymes, clickedRhyme);
+
+    const stateUpdates = {
+      text,
+      selectedRhymeIndex
+    }
+
+    return Object.assign({}, state, stateUpdates)
+  }
+
+}
+
+function chooseRhymeAtIndex (availableRhymes, clickedRhyme) {
+  return _.indexOf(availableRhymes, clickedRhyme)
+}
