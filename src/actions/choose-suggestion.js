7cc3540cda8984ebbcbd0ea51b1379d63bb4dcd4
Raquel Moss <raquelxmoss@gmail.com>
2016-01-05 20:03:10 +1300

    Small tidy up

--- src/actions/choose-suggestion.js
+++ src/actions/choose-suggestion.js
@@ -2,9 +2,8 @@ import _ from 'lodash';
 
 import { appendNextRhyme } from '../helpers';
 
-export default function chooseSuggestion (event) {
+export default function chooseSuggestion (clickedRhyme) {
   return state => {
-    const clickedRhyme = event.target.innerText;
     const text = appendNextRhyme(false, state.text, clickedRhyme);
     const selectedRhymeIndex = chooseRhymeAtIndex(state.availableRhymes, clickedRhyme);
 
